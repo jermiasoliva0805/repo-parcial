30143dbe9d21f47edfd31485b724e0d8666463dd
JonathanES9 <jonathan.stubbe9@hotmail.com>
2025-09-26 16:52:21 -0300

    Se creo automovilAplicaction y su interface

---
+++ Backend/Application/ApplicationServices/AutomovilApplicationService.cs
@@ -0,0 +1,27 @@
+﻿using Application.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ApplicationServices
+{
+    public class AutomovilApplicationService : IAutomovilApplicationService
+    {
+        private readonly IAutomovilRepository _automovilRepository;
+        public AutomovilApplicationService(IAutomovilRepository automovilRepository)
+        {
+            _automovilRepository = automovilRepository ?? throw new
+           ArgumentNullException(nameof(automovilRepository));
+        }
+        public bool AutomovilExist(string numeroChasis)
+        {
+            if (string.IsNullOrWhiteSpace(numeroChasis))
+                throw new ArgumentNullException(nameof(numeroChasis));
+            return _automovilRepository.Query()
+            .Any(a => a.NumeroChasis == numeroChasis);
+        }
+    }
+
+}
